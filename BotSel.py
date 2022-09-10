5d56790df2ea9e197bb4f3f3dbf8a86b10df8624
Zack <zakaria.janah2018@gmail.com>
2022-09-10 14:41:16 +0100

    third

--- BotSel.py
+++ BotSel.py
@@ -1,25 +1,51 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains as A
 from lxml import html
 from time import sleep
 import csv
+import openpyxl
+import pandas as pd
+
 
 num=0
 f=open("produit.csv","a",newline="")
-tup1=("Numero","Titre","price","Offre","Nb_piece_vendu","Etoile","Envoi","Boutique")
+tup1=("Titre","price","Offre","Nb_piece_vendu","Etoile","Envoi","Boutique")
 writer=csv.writer(f)
 writer.writerow(tup1)
 driver=webdriver.Chrome('chromedriver')
 
 prod=input("entez un produit:")
-
-for page in range(1,10):
+driver.maximize_window()
+for page in range(1,2):
     driver.get(f'https://fr.aliexpress.com/wholesale?trafficChannel=main&d=y&CatId=0&SearchText={prod}&ltype=wholesale&SortType=default&page={page}')
-    sleep(2)
+    sleep(1)
+
+    for i in range(24):
+        driver.execute_script('window.scrollBy(0,200)',"")
+        sleep(3)
+    driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    sleep(1)
 
     content=html.fromstring(driver.page_source)
 
-    driver.execute_script('window.scrollTo(0,document.body.scrollHeight);')
-    sleep(2)
+
+    # elem = driver.find_element(By.TAG_NAME, 'body')
+    #
+    # i = 0
+    # while i < 9:
+    #     elem.send_keys(Keys.PAGE_DOWN)
+    #     sleep(1)
+    #     i += 1
+    # sleep(10)
+    # for i in range(24):
+    #     driver.execute_script('window.scrollBy(0,200)',"")
+    #     sleep(3)
+    # driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    # sleep(1)
+
+
 
 
     for produit in content.xpath('//a[@class="_3t7zg _2f4Ho"]'):
@@ -29,9 +55,11 @@ for page in range(1,10):
         if len(Prix)==2:
             Prix=Prix[0]+Prix[1]
             Price.append(Prix)
-        else:
+        elif len(Prix)==4:
             Prix=Prix[0]+Prix[1]+Prix[2]+Prix[3]
             Price.append(Prix)
+        else:
+            pass
         Offre=produit.xpath('.//div[@class="g_XRl"]/span[1]/text()')
         if (len(Offre)==0):
             Offre=['aucune offre']
@@ -48,8 +76,12 @@ for page in range(1,10):
         if (len(Boutique)==0):Boutique=['UNKNOWN']
 
         num+=1
-        tup = (num,Titre[0], Price[0], Offre[0], Nb_piece_vendu[0], Etoile[0], Envoi[0], Boutique[0])
+        tup = (Titre[0], Price[0], Offre[0], Nb_piece_vendu[0], Etoile[0], Envoi[0], Boutique[0])
         writer.writerow(tup)
         print(tup)
 
 f.close()
+
+# readfile=pd.read_csv("produit.csv")
+#
+# readfile.to_excel("produit.xlsx",index=None,header=True)
