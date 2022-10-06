a7bb4b9a93cac8da2d1408d112bf2de86d03072d
Zack <zakaria.janah2018@gmail.com>
2022-10-06 10:01:12 +0100

    fourth

--- BotSel.py
+++ BotSel.py
@@ -7,24 +7,51 @@ from time import sleep
 import csv
 import openpyxl
 import pandas as pd
+import threading
+
+df=pd.DataFrame(columns=[
+        "Title",
+        "Price",
+        "Offre",
+        "Nb_piece_vendu",
+        "Etoile",
+        "Envoi",
+        "Boutique"
+])
+
+list_thread=[]
+
+
 
 
 num=0
-f=open("produit.csv","a",newline="")
+f=open("test.csv","a",newline="")
 tup1=("Titre","price","Offre","Nb_piece_vendu","Etoile","Envoi","Boutique")
 writer=csv.writer(f)
 writer.writerow(tup1)
-driver=webdriver.Chrome('chromedriver')
-
+# driver=webdriver.Chrome('chromedriver')
+#
 prod=input("entez un produit:")
-driver.maximize_window()
-for page in range(1,2):
+# driver.maximize_window()
+# for page in range(1,2):
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+
+
+
+def scrap(page):
+    list_product=[]
+    driver = webdriver.Chrome('chromedriver',options=op)
+
+    # prod = input("entez un produit:")
+    driver.maximize_window()
+    num=0
     driver.get(f'https://fr.aliexpress.com/wholesale?trafficChannel=main&d=y&CatId=0&SearchText={prod}&ltype=wholesale&SortType=default&page={page}')
     sleep(1)
 
     for i in range(24):
         driver.execute_script('window.scrollBy(0,200)',"")
-        sleep(3)
+        sleep(1)
     driver.execute_script('window.scrollTo(0,document.body.scrollHeight)')
     sleep(1)
 
@@ -76,12 +103,55 @@ for page in range(1,2):
         if (len(Boutique)==0):Boutique=['UNKNOWN']
 
         num+=1
-        tup = (Titre[0], Price[0], Offre[0], Nb_piece_vendu[0], Etoile[0], Envoi[0], Boutique[0])
+        # d ={
+        #     "Title":[Titre],
+        #     "Price":[Price],
+        #     "Offre":[Offre],
+        #     "Nb_piece_vendu":[Nb_piece_vendu],
+        #     "Etoile":[Etoile],
+        #     "Envoi":[Envoi],
+        #     "Boutique":[Boutique]
+        # }
+        # x=x.append({"Title":[Titre],"Price":[Price],"Offre":[Offre],"Nb_piece_vendu":[Nb_piece_vendu],"Etoile":[Etoile],"Envoi":[Envoi],"Boutique":[Boutique]},ignore_index=True)
+        tup=(Titre[0], Price[0], Offre[0], Nb_piece_vendu[0], Etoile[0], Envoi[0], Boutique[0])
         writer.writerow(tup)
         print(tup)
+#    return list_product
 
-f.close()
+# f.close()
 
 # readfile=pd.read_csv("produit.csv")
 #
 # readfile.to_excel("produit.xlsx",index=None,header=True)
+
+
+
+
+
+for n in range(1,7):
+    t = threading.Thread(target=scrap, args=(n,))
+    list_thread.append(t)
+    t.start()
+
+# wait for the threads to complete
+for t in list_thread:
+    t.join()
+
+list_thread=[]
+for n in range(7,13):
+    t = threading.Thread(target=scrap, args=(n,))
+    list_thread.append(t)
+    t.start()
+
+# wait for the threads to complete
+for t in list_thread:
+    t.join()
+
+f.close()
+        # df=df.append(x,ignore_index=True)
+# df.to_csv('test.csv')
+# for i in range(1,6):
+#     list_thread.append(threading.Thread(target=scrap,args=(i,)))
+#
+# for j in range(len(list_thread)):
+#     list_thread[j].start()
